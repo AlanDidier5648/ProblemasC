066f6ee66c001aa9c7e60818ec1fb6e461c4650e
AlanDidier5648 <saed17140@gmail.com>
2025-03-13 00:22:54 -0600

    comentarios años

--- problema1.c
+++ problema1.c
@@ -1,3 +1,13 @@
+/*
+Transformar años a meses 
+meses a semanas 
+semanas a dias
+y dias a horas
+
+
+Pedir al usuario que digite la cantidad de años conas las que se hara la conversion 
+*/
+
 #include <stdio.h>
 
 int main(){
