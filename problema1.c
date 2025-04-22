450e1f54e2e33d87fe787a83a5a6781b881cdc5a
AlanDidier5648 <saed17140@gmail.com>
2025-04-21 23:08:02 -0600

    commit problema2

--- problema1.c
+++ problema1.c
@@ -4,6 +4,11 @@ meses a semanas
 semanas a dias
 y dias a horas
 
+1 año = 12 meses
+1 mes  = 30 dias
+1 semama = 7 dias
+1 dia = 24 horas
+
 
 Pedir al usuario que digite la cantidad de años conas las que se hara la conversion 
 */
@@ -12,8 +17,20 @@ Pedir al usuario que digite la cantidad de años conas las que se hara la conver
 
 int main(){
 
-    printf("hola mundo.");
-    printf("hola");
+    int edad,meses,semanas,dias, horas;
+
+    printf("Digita tu edad:");
+    scanf("%i",&edad);
+
+    meses = edad*12;
+    semanas = meses*4;
+    dias = semanas*7;
+    horas = dias*24;
+
+    printf("Tu edad en meses es de %i\n",meses);
+    printf("Tu edad en semanas es de %i\n:",semanas);
+    printf("Tu edad en dias es de %i\n:",dias);
+    printf("Tu edad en horas es de %i\n:",horas);
 
     return 0;
 }
---
+++ problema2.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+/*
+Hacer un programa que simule un  cajero automatico con un saldo inicial
+*/
+
+int main(int argc, char const *argv[]){
+
+    int opc;
+
+    printf("Ingresa una opcion:");
+    scanf("%d",&opc);
+
+    switch (opc){
+    case 1:
+        /* code */
+        break;
+    
+    default:
+        break;
+    }
+    
+    return 0;
+}
